e9c4ffb0a2b1cfe29c29386685b4b078e5b3117c
Brandon Jorgensen <Brandonjorgen@gmail.com>
2018-09-17 15:49:27 -0600

    Variables Lecture

---
+++ 1610/Assets/Variables.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+
+	public int AmmoCount = 100;
+	public int InventoryCount;
+	
+	public float Health = 1.0f;
+	public float Money;
+	
+	public string PlayerName;
+	public string Team = "Wolves";
+
+	public Vector3 Location;
+
+	public bool IsAWizard;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		print(PlayerName);
+		print("has");
+		print("$" + Money);
+
+		if (IsAWizard)
+		{
+			print(PlayerName + " IS A WIZARD!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Location;
+	}
+}
