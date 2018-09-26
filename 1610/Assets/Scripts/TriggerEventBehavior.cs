f75e3015f7ffecc80e1470823fdfc8328134b606
Brandon Jorgensen <Brandonjorgen@gmail.com>
2018-09-26 15:44:19 -0600

    Trigger Lecture

---
+++ 1610/Assets/Scripts/TriggerEventBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+
+	public UnityEvent Enable;
+	public UnityEvent Trigger;
+	
+	private void OnEnable()
+	{
+		Enable.Invoke();
+	}
+
+	private void OnTriggerEnter()
+	{
+		Trigger.Invoke();
+	}
+}
