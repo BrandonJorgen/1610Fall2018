dbc1615b08a60e2d1d0df365df170c70982870eb
Brandon Jorgensen <Brandonjorgen@gmail.com>
2018-10-03 15:46:17 -0600

    Character Controller Lecture

---
+++ 1610/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    private CharacterController controller;
+    public float Gravity = 9.81f;
+    public float JumpSpeed = 3.0f;
+    public float MoveSpeed = 3.0f;
+    public float RotateSpeed = 3.0f;
+    private Vector3 position;
+    private Vector3 rotation;
+    
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            
+            position.Set(0, 0, MoveSpeed * Input.GetAxis("Vertical"));
+            transform.Rotate(rotation);
+            rotation.Set(0, RotateSpeed * Input.GetAxis("Horizontal"), 0);
+            position = transform.TransformDirection(position);
+            
+            if (Input.GetButton("Jump"))
+            {
+                position.y = JumpSpeed;
+            }
+        }
+        position.y -= Gravity * Time.deltaTime;
+        controller.Move(position * Time.deltaTime);
+    }
+}
