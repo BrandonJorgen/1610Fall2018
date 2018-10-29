448b067c17057cdf21417323d95b58ae6307512d
Brandon Jorgensen <Brandonjorgen@gmail.com>
2018-10-29 15:06:26 -0600

    Lecture - Enum Switch Statements

---
+++ 1610/Assets/Scripts/SwitchSatements.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchSatements : MonoBehaviour
+{
+
+	public GameStates.States GameState;
+	public UnityEvent Loading, Starting, Playing, Paused, Ending;
+	
+	// Update is called once per frame
+	void Update () {
+		switch (GameState)
+		{
+			case GameStates.States.Loading:
+				print("We are loading the game!");
+				break;
+			
+			case GameStates.States.Starting:
+				print("We are starting the game!");
+				break;
+			
+			case GameStates.States.Playing:
+				print("We are playing the game!");
+				break;
+			
+			case GameStates.States.Paused:
+				print("The game is paused!");
+				break;
+			
+			case GameStates.States.Ending:
+				print("We are ending the game!");
+				break;
+		}
+	}
+}
