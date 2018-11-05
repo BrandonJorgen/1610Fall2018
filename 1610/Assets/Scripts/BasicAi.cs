b9423c35504272dda8cf8133be5a9e0a59c29ffe
Brandon Jorgensen <Brandonjorgen@gmail.com>
2018-11-05 15:08:57 -0700

    Lecture - NavMesh Navigation

---
+++ 1610/Assets/Scripts/BasicAi.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicAi : MonoBehaviour
+{
+
+	public Transform Destination;
+	private NavMeshAgent agent;
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update ()
+	{
+		if (Destination != null) agent.destination = Destination.position;
+	}
+}
