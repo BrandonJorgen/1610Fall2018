4f0b20fded3043bc1941b4dcb45381cb6dab8c3b
Unknown <Brandonjorgen@gmail.com>
2018-11-09 20:38:37 -0700

    Game Work

---
+++ 1610/Assets/Scripts/ImprovedZeldaTwo/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+	private CharacterController controller;
+	public float Gravity = 9.81f;
+	public float JumpSpeed = 3.0f;
+	public float MoveSpeed = 3.0f;
+	private Vector3 position;
+	private Vector3 rotation;
+    
+	private void Start()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+
+	private void Update()
+	{
+		if (controller.isGrounded)
+		{
+            
+			position.Set(0, 0, MoveSpeed * Input.GetAxis("Vertical"));
+			position = transform.TransformDirection(position);
+            
+			if (Input.GetButton("Jump"))
+			{
+				position.y = JumpSpeed;
+			}
+		}
+		position.y -= Gravity * Time.deltaTime;
+		controller.Move(position * Time.deltaTime);
+	}
+}
