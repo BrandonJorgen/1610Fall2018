5933ab1fef21dc36dacc5ee9512863cd3ca41cc4
Unknown <Brandonjorgen@gmail.com>
2018-11-11 16:29:18 -0700

    Game Work
    Started on Attack Scripting

--- 1610/Assets/Scripts/ImprovedZeldaTwo/Scripts/PlayerControl.cs
+++ 1610/Assets/Scripts/ImprovedZeldaTwo/Scripts/PlayerControl.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerControl : MonoBehaviour
 {
 	private CharacterController controller;
+	public UnityEvent AttackDown, AttackUp, UpwardAttackDown, UpwardAttackUp;
 	public float Gravity = 9.81f;
 	public float JumpSpeed = 3.0f;
 	public float MoveSpeed = 3.0f;
@@ -29,5 +31,30 @@ public class PlayerControl : MonoBehaviour
 		}
 		position.y -= Gravity * Time.deltaTime;
 		controller.Move(position * Time.deltaTime);
+
+		if (Input.GetAxis("Horizontal") < 0)
+		{
+			//ROTATION ON Z AXIS HERE
+		}
+
+		if (Input.GetButton("Fire3"))
+		{
+			AttackDown.Invoke();
+			Debug.Log("You are pressing Fire3");
+		}
+		else
+		{
+			AttackUp.Invoke();
+		}
+
+		if (Input.GetAxis("Vertical") > 0 && Input.GetButton("Fire3"))
+		{
+			UpwardAttackDown.Invoke();
+			Debug.Log("You are Pressing Up and Attack");
+		}
+		else
+		{
+			UpwardAttackUp.Invoke();
+		}
 	}
 }
