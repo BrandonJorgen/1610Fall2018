0cbe194b203d471f5f8710afed7d05db79a2ed89
Unknown <Brandonjorgen@gmail.com>
2018-11-21 21:51:37 -0700

    Game Work
    Added Full HP Text to pickups

--- 1610/Assets/Scripts/ImprovedZeldaTwo/Scripts/ValueCheckAndEvent.cs
+++ 1610/Assets/Scripts/ImprovedZeldaTwo/Scripts/ValueCheckAndEvent.cs
@@ -4,8 +4,13 @@ using UnityEngine.Events;
 
 public class ValueCheckAndEvent : MonoBehaviour
 {
-    public UnityEvent TriggerEnterNegativeValueCheck;
+    public UnityEvent TriggerEnterNegativeValueCheck, TriggerEnterPositiveValueCheck, TriggerExit, OnStart;
     public FloatData Value;
+
+    private void Start()
+    {
+        OnStart.Invoke();
+    }
 
     private void OnTriggerEnter()
     {
@@ -13,5 +18,17 @@ public class ValueCheckAndEvent : MonoBehaviour
         {
             TriggerEnterNegativeValueCheck.Invoke();
         }
+
+        if (Value.Value >= 1)
+        {
+            TriggerEnterPositiveValueCheck.Invoke();
+            Debug.Log("Value is reported as greater than or equal to 1");
+        }
+    }
+
+    private void OnTriggerExit()
+    {
+        TriggerExit.Invoke();
+        Debug.Log("You have left the trigger zone");
     }
 }
