35dac46c0b1da53ad9d348ad74e314f18053c0a8
Unknown <Brandonjorgen@gmail.com>
2018-12-01 00:27:24 -0700

    Game Work

---
+++ 1610/Assets/Scripts/CaveExplorer/Scripts/FailureEvents.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FailureEvents : MonoBehaviour
+{
+
+	public UnityEvent PlayerDeath;
+	public FloatData ValueOne, ValueTwo;
+
+	private void ZeroHealth()
+	{
+		if (ValueOne == ValueTwo)
+		{
+			PlayerDeath.Invoke();
+		}
+	}
+	
+}
